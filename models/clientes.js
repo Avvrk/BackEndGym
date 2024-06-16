8f1c45313bac850e24376b50742dbe4da1a03177
Jose M. Vargas <137236209+Avvrk@users.noreply.github.com>
2024-06-16 16:38:08 -0500

    Update clientes.js

--- models/clientes.js
+++ models/clientes.js
@@ -13,7 +13,7 @@ const clienteSchema = new mongoose.Schema({
     observaciones: { type: String },
     estado: { type: Number, default: 1 },
     plan: { type: String },
-    idPlan: { type: mongoose.Schema.Types.ObjectId, ref: "plane", required: true  },
+    _idPlan: { type: mongoose.Schema.Types.ObjectId, ref: "plane", required: true  },
     fechaVencimiento: { type: Date },
     foto: { type: String },
     seguimiento: [
