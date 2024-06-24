7e3af16ffa08b40f1103348d90d100bcc0e06fdd
Jose M. Vargas <137236209+Avvrk@users.noreply.github.com>
2024-06-24 11:02:06 -0500

    Update pagos.js

--- routes/pagos.js
+++ routes/pagos.js
@@ -9,7 +9,7 @@ const router = Router();
 
 router.get('/', validarJWT, httpPagos.getPagos);
 
-router.get('/fechas/:fecha', [
+router.get('/fechainicio/:fechaInicio/fechafin/:fechaFin', [
     check('fechaInicio', 'La fecha de inicio es requerida.').notEmpty(),
     check('fechaInicio', 'La fecha de inicio debe ser una fecha válida.').isISO8601().toDate(),
     check('fechaFin', 'La fecha de fin es requerida.').notEmpty(),
